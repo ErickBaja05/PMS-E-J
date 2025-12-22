96ff6dee9c53b9d2b0147f41c71855e3b62a4ada
Erick Bajaña <erick.bajana@epn.edu.ec>
2025-12-22 03:35:25 -0500

    feat: Auditoria Terminada e Inicio de Kardex

---
+++ src/main/java/com/grupo2/PMSEYJ/inventarioYProductos/controller/registrarProducto.java
@@ -0,0 +1,43 @@
+package com.grupo2.PMSEYJ.inventarioYProductos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class registrarProducto {
+
+    @FXML
+    private Button btnRegistrarCliente;
+
+    @FXML
+    private Label lblMensaje;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtNombre1;
+
+    @FXML
+    private TextField txtNombre11;
+
+    @FXML
+    private TextField txtNombre111;
+
+    @FXML
+    private TextField txtNombre2;
+
+    @FXML
+    private TextField txtNombre3;
+
+    @FXML
+    private TextField txtNombre4;
+
+    @FXML
+    void crearProducto(ActionEvent event) {
+
+    }
+
+}
